dcc86bdd554211f22cc1232910d4bc2f25d60ec1
Rhythm-2829 <141955301+Rhythm-2829@users.noreply.github.com>
2024-07-08 22:39:56 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 //placing user order from frontend
 const placeOrder = async (req,res) => {
 
-    const frontend_url = "http://localhost:5174";
+    const frontend_url = "https://tomato-frontend-c8cg.onrender.com";
 
     try {
         const newOrder = new orderModel({
